efc21c9d5bdc24385fa3d82b510b48966fbfc3a4
Julien Marcesse <35881301+mercure84@users.noreply.github.com>
2019-05-10 21:20:53 +0200

    Update ProduitGratuitException.java

--- src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
+++ src/main/java/com/ecommerce/microcommerce/web/exceptions/ProduitGratuitException.java
@@ -2,7 +2,7 @@ package com.ecommerce.microcommerce.web.exceptions;
 
 public class ProduitGratuitException extends RuntimeException {
 
-
+    @@ResponseStatus(HttpStatus.BAD_REQUEST)
     public ProduitGratuitException(String s) {
         super(s);
     }
